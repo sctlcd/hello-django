2c3b712bdcf89fe2633b57713597b2440677324e
8 <lucinda.souchet@outlook.com>
2020-09-15 18:11:53 +0100

    add tests in tests_views.py and tests_models.py

--- todo/tests_views.py
+++ todo/tests_views.py
@@ -2,7 +2,6 @@ from django.test import TestCase
 from .models import Item
 
 
-# Create your tests here.
 class TestViews(TestCase):
 
     def test_get_todo_list(self):
@@ -38,3 +37,11 @@ class TestViews(TestCase):
         self.assertRedirects(response, '/')
         updated_item = Item.objects.get(id=item.id)
         self.assertFalse(updated_item.done)
+
+    def test_can_edit_item(self):
+        item = Item.objects.create(name='Test Todo Item')
+        response = self.client.post(f'/edit/{item.id}',
+                    {'name': 'Updated Name'})
+        self.assertRedirects(response, '/')
+        updated_item = Item.objects.get(id=item.id)
+        self.assertEqual(updated_item.name, 'Updated Name')
