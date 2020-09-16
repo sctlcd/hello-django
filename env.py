37602b4cf8177133d87adc1ee0c08bc19fc980bb
8 <lucinda.souchet@outlook.com>
2020-09-16 19:25:13 +0100

    remove secret key

--- env.py
+++ env.py
@@ -1,5 +1,6 @@
 import os
 
-os.environ["DEVELOPMENT"] = "postgres://eipsbypatativp:4ec9c5cde96c9e080b8eb0 \
+os.environ['DEVELOPMENT'] = 'postgres://eipsbypatativp:4ec9c5cde96c9e080b8eb0 \
     d7413b9215003aee99db867f0ff359d95dccce4875@ec2-54-75-229-28.eu-west-1. \
-    compute.amazonaws.com:5432/d5f6frl9i1ugg7"
+    compute.amazonaws.com:5432/d5f6frl9i1ugg7'
+os.environ['SECRET_KEY'] = 'w5!n(mv!wlk1o2=f5n&m4+2b6*a5p9lt!cm16ljkzq8y7yzpvt'
